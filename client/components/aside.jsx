320421b583925c43a615879e79a54c7fd23d6be3
andrewlo42 <andrewclo42@gmail.com>
2020-02-19 23:12:56 +0000

    implemented an aside

---
+++ client/components/aside.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Aside(props) {
+  return (
+    <div className='aside'>
+      <a href="https://www.linkedin.com/in/AndrewCLo/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="decoration-none default-nav contact-linkedin pointer mr-2"
+        aria-label="LinkedIn">
+        <div className="d-flex">
+          <i className="fab fa-linkedin aside-icon m-auto" />
+        </div>
+      </a>
+      <a href="https://github.com/andrewlo42"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mt-2 decoration-none default-nav contact-github pointer mr-2"
+        aria-label="Github">
+        <div className="border-div-small d-flex">
+          <i className="fab fa-github aside-icon m-auto" />
+        </div>
+      </a>
+    </div>
+
+  );
+}
+
+export default Aside;
---
+++ client/components/scroll-indicator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ScrollIndicator(props) {
+  return (
+    <div className="scroll">
+      <p>
+        Scroll
+      </p>
+    </div>
+  );
+}
+
+export default ScrollIndicator;
