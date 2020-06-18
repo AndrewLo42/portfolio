7a21ff9005b908221f264accbe2e5e1454c0e23c
andrewlo42 <andrewclo42@gmail.com>
2020-06-18 21:27:08 +0000

    updated resume, cleaned up home page

--- client/components/welcome.jsx
+++ client/components/welcome.jsx
@@ -7,47 +7,22 @@ import {
 // import CarouselComponent from './carousel';
 
 class Welcome extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      carouselSlides: [
-        {
-          src: './images/nair.jpg',
-          altText: 'Compete',
-          caption: 'Software Engineer'
-        },
-        {
-          src: './images/foxhmm.jpg',
-          altText: 'hmm',
-          caption: 'Creator'
-        },
-        {
-          src: './images/knee.gif',
-          altText: 'knee',
-          caption: 'Innovator'
-        }
-      ]
-    };
-  }
 
   render() {
     return (
       <div className={` welcome-page ${this.props.mode}`}>
         <h1 className={`ml-4 welcome-message ${this.props.mode}`}>Life is this crazy, mystical thing...</h1>
+        <Container className='p-2'>
+          <Col>
+            <div className={`ml-4 mt-2 p-1 welcome-title ${this.props.mode}`}>
+              <h1>My name is Andrew Lo</h1>
+              <div className={'welcome-footer'}>I&#39;m a programmer with a mission to learn everything and create elegant solutions in a digital world.</div>
+            </div>
+          </Col>
+          <Row className="row justify-content-center">
+          </Row>
+        </Container>
         <div className="welcome-container">
-          <Container className='p-2'>
-            <Col>
-              <div className={`ml-4 mt-2 p-1 welcome-title ${this.props.mode}`}>
-                <h1>My name is Andrew Lo</h1>
-                <div className={'welcome-footer'}>I&#39;m a programmer with a mission to learn everything and create elegant solutions in a digital world.</div>
-              </div>
-            </Col>
-            <Row className="row justify-content-center">
-              {/* <CarouselComponent slides={this.state.carouselSlides} mode={this.props.mode}/> */}
-            </Row>
-          </Container>
-
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fillOpacity="1" d="M0,224L120,240C240,256,480,288,720,266.7C960,245,1200,171,1320,133.3L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
         </div>
         <div className={`row justify-content-center menu-links ${this.props.mode}`}>
           <a href="https://www.linkedin.com/in/AndrewCLo/"
@@ -78,6 +53,8 @@ class Welcome extends React.Component {
             </div>
           </a>
         </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fillOpacity="1" d="M0,224L120,240C240,256,480,288,720,266.7C960,245,1200,171,1320,133.3L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
+
       </div>
     );
   }
